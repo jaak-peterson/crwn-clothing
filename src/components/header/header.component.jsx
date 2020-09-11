25330888ca3b62d3e2f0d89a38dda91e4b439f07
Jaak Peterson <j.peterson@amenti.ch>
2020-09-11 13:08:41 +0300

    Implement code that pulls in our shop data from firestore

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,39 +4,40 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { createStructuredSelector } from 'reselect';
 
-import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selector';
 
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionDiv, OptionLink } from './header.styles';
+
 const Header = ({ currentUser, hidden }) => (
-   <div className='header'>
-    <Link className='logo-container' to="/">
+   <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className='logo'></Logo>
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to='/shop'>
         SHOP
-      </Link>
-      <Link className='option' to='/shop'>
+      </OptionLink>
+      <OptionLink to='/shop'>
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ? 
-        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+        <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
         :
-        <Link className='option' to='/signin'>SIGN IN</Link>
+        <OptionLink className='option' to='/signin'>SIGN IN</OptionLink>
       }
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {
       hidden ?
       null :
       <CartDropdown />
     }
-   </div>
+   </HeaderContainer>
 )
 
 // state is redux root state
